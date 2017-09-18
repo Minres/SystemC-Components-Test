7c32c6aab218111a8760ce2d2218225b92073772
Eyck Jentzsch <eyck@minres.com>
2017-09-18 12:18:55 +0200

    License cleanup

--- examples/simple_system/sc_main.cpp
+++ examples/simple_system/sc_main.cpp
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Copyright 2017 ubuntu
+// Copyright 2017 eyck@minres.com
 // 
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
 // use this file except in compliance with the License.  You may obtain a copy
@@ -17,7 +17,7 @@
  * sc_main.cpp
  *
  *  Created on: 17.09.2017
- *      Author: ubuntu
+ *      Author: eyck@minres.com
  */
 
 #include "simple_system.h"
